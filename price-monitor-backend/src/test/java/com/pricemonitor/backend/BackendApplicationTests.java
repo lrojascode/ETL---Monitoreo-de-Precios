fe1996626ddac329ab1aac3fc73dee9dc26d5594
luisr <luisr@example.com>
2026-05-28 00:56:51 -0400

    test(backend): add test configurations for spring boot API

---
+++ price-monitor-backend/src/test/java/com/pricemonitor/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pricemonitor.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
